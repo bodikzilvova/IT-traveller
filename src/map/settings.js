3d8e3e64264b3547983ccada2da64bc6844dce9f
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2024-09-12 16:26:46 +0100

    update ignore

--- src/map/settings.js
+++ src/map/settings.js
@@ -1,6 +1,6 @@
-import { token } from './token'
+// import { token } from './token'
 
 export const mapSettings = {
   style: 'mapbox://styles/mapbox/streets-v12',
-  apiToken: token
+  apiToken: import.meta.env.VITE_MAP_TOKEN
 }
