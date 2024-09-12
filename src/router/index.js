e74772b8d5947e948083faf50d3826a0802d140b
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2024-09-12 18:27:31 +0100

    checking beforeEach

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,3 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-// import { authService } from '@/api/authService'
-
-// const GretingPage = () => import('@/views/GreetingView.vue')
-// const HomePage = () => import('@/views/HomepageView.vue')
-// const AuthPage = () => import('@/views/AuthView.vue')
-// const LoginPage = () => import('@/views/LoginView.vue')
-// const RegistrationPage = () => import('@/views/RegistrationView.vue')
-
-// const routes = [
-//   { path: '/', component: GretingPage, name: 'greeting' },
-//   { path: '/map', component: HomePage, name: 'homepage' },
-//   {
-//     path: '/auth',
-//     component: AuthPage,
-//     redirect: '/auth/login',
-//     children: [
-//       { path: 'login', component: LoginPage, name: 'login' },
-//       { path: 'registration', component: RegistrationPage, name: 'registration' }
-//     ]
-//   }
-// ]
-
-// export const router = createRouter({
-//   history: createWebHistory('/IT-traveller/'),
-//   routes
-// })
-
-// router.beforeEach((to, from, next) => {
-//   const authRoutes = ['login', 'registration']
-//   const { name } = to
-
-//   if (authService.isLoggedIn() && authRoutes.includes(name)) {
-//     next({ name: 'homepage' })
-//   } else if (!authRoutes.includes(name) && !authService.isLoggedIn()) {
-//     next({ name: 'login' })
-//   } else {
-//     next()
-//   }
-// })
-
 import { createRouter, createWebHistory } from 'vue-router'
 import { authService } from '@/api/authService'
 
@@ -67,15 +26,15 @@ export const router = createRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  const authRoutes = ['login', 'registration']
-  const { name } = to
+// router.beforeEach((to, from, next) => {
+//   const authRoutes = ['login', 'registration']
+//   const { name } = to
 
-  if (authService.isLoggedIn() && authRoutes.includes(name)) {
-    next({ name: 'homepage' })
-  } else if (!authRoutes.includes(name) && !authService.isLoggedIn()) {
-    next({ name: 'login' })
-  } else {
-    next()
-  }
-})
+//   if (authService.isLoggedIn() && authRoutes.includes(name)) {
+//     next({ name: 'homepage' })
+//   } else if (!authRoutes.includes(name) && !authService.isLoggedIn()) {
+//     next({ name: 'login' })
+//   } else {
+//     next()
+//   }
+// })
